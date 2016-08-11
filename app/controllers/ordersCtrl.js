b30fdab32c436e9835bc8f8ef6014c450bdc3bbf
jadenj13 <jadenj13@hotmail.com>
2016-08-10 19:34:27 -0600

    working on removing from cart

--- app/controllers/ordersCtrl.js
+++ app/controllers/ordersCtrl.js
@@ -12,10 +12,11 @@ module.exports = {
 	},
 
 	deleteFromCart: function(req, res) {
-		User.findByIdAndUpdate(req.params.user_id, {$pull: {cart: {item: {$oid: req.body}}}}, function(err, result) {
+		console.log(req.body);
+		User.findByIdAndUpdate(req.params.user_id, {$pull: {cart: req.body}}, function(err, result) {
 			if (err) return res.status(500).send(err);
 			else res.send(result);
-		});
+		})
 	}
 
 };
