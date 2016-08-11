11cd049c3a88a4458ec4ac5b2a9135f285751c35
jadenj13 <jadenj13@hotmail.com>
2016-08-11 17:32:55 -0600

    working on removing from cart

--- app/controllers/ordersCtrl.js
+++ app/controllers/ordersCtrl.js
@@ -2,40 +2,39 @@
 
 var User = require('../models/userModel');
 
-	function postToCart(req, res) {
-		User.findByIdAndUpdate(req.params.user_id, {$push: {cart: req.body}}, function(err, result) {
-			if (err) return res.status(500).send(err);
-			else res.send(result);
-		});
-	}
-
-	function deleteFromCart(req, res) {
-		User.findById(req.params._id, function(err, result) {
-			if (err) { res.status(500).send(err) }
-			var myUser = res;
-			var foundItem = -1;
-			myUser.cart.forEach(function(cartItem, index) {
-				if (cartItem._id.toString() === req.body) {
-					foundItem = index;
-				}
-			});
-			if (foundItem >= 0) {
-				console.log('Found Item: ', foundItem);
-				myUser.cart.splice(foundItem, 1);
-			}
-			saveUser(myUser, req, res);
-		});
-		function saveUser(userToSave, req, res) {
-			userToSave.save(function(err, result) {
-				if (err) { res.status(500).send(err) }
-				else { res.send(result) }
-			})
-		}
-	}
-
-	module.exports = {
-		postToCart: postToCart,
-		deleteFromCart: deleteFromCart
-	}
-
+function postToCart(req, res) {
+	User.findByIdAndUpdate(req.params.user_id, {$push: {cart: req.body}}, function(err, result) {
+		if (err) return res.status(500).send(err);
+		else res.send(result);
+	});
 }
+
+function deleteFromCart(req, res) {
+	User.findById(req.params._id, function(err, result) {
+		if (err) { res.status(500).send(err) }
+		var myUser = res;
+		var foundItem = -1;
+		myUser.cart.forEach(function(cartItem, index) {
+			if (cartItem._id.toString() === req.body) {
+				foundItem = index;				
+			}
+		});
+		if (foundItem >= 0) {
+			console.log('Found Item: ', foundItem);
+			myUser.cart.splice(foundItem, 1);
+		}
+		saveUser(myUser, req, res);
+	});
+	function saveUser(userToSave, req, res) {
+		userToSave.save(function(err, result) {
+			if (err) { res.status(500).send(err) }
+			else { res.send(result) }
+		})
+	}
+}
+
+module.exports = {
+	postToCart: postToCart,
+	deleteFromCart: deleteFromCart
+}
+
