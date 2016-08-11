d5978af531895bbef2294bb9793634e5a1bc6b1b
jadenj13 <jadenj13@hotmail.com>
2016-08-11 17:34:50 -0600

    working on removing from cart

--- app/controllers/ordersCtrl.js
+++ app/controllers/ordersCtrl.js
@@ -10,27 +10,28 @@ function postToCart(req, res) {
 }
 
 function deleteFromCart(req, res) {
-	User.findById(req.params._id, function(err, result) {
-		if (err) { res.status(500).send(err) }
-		var myUser = res;
-		var foundItem = -1;
-		myUser.cart.forEach(function(cartItem, index) {
-			if (cartItem._id.toString() === req.body) {
-				foundItem = index;				
-			}
-		});
-		if (foundItem >= 0) {
-			console.log('Found Item: ', foundItem);
-			myUser.cart.splice(foundItem, 1);
-		}
-		saveUser(myUser, req, res);
-	});
-	function saveUser(userToSave, req, res) {
-		userToSave.save(function(err, result) {
-			if (err) { res.status(500).send(err) }
-			else { res.send(result) }
-		})
-	}
+	// User.findById(req.params._id, function(err, result) {
+	// 	if (err) { res.status(500).send(err) }
+	// 	var myUser = res;
+	// 	var foundItem = -1;
+	// 	myUser.cart.forEach(function(cartItem, index) {
+	// 		if (cartItem._id.toString() === req.body) {
+	// 			foundItem = index;				
+	// 		}
+	// 	});
+	// 	if (foundItem >= 0) {
+	// 		console.log('Found Item: ', foundItem);
+	// 		myUser.cart.splice(foundItem, 1);
+	// 	}
+	// 	saveUser(myUser, req, res);
+	// });
+	// function saveUser(userToSave, req, res) {
+	// 	userToSave.save(function(err, result) {
+	// 		if (err) { res.status(500).send(err) }
+	// 		else { res.send(result) }
+	// 	})
+	// }
+	res.status(500)
 }
 
 module.exports = {
